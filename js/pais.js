02dc222984bd7ac8f805a2b91fc2657e94adfe5a
uo287791 <uo287791@uniovi.es>
2023-11-22 09:05:23 +0100

    VIVA ESPAÑA

--- js/pais.js
+++ js/pais.js
@@ -1,25 +1,24 @@
 "use strict";
 class Pais {
 
-    nombre;
-    capital;
-    poblacion;
-    formaDeGobierno;
-    coordenadasCapital;
-    religion;
+    key = "c671d920e7a1bac668637655ee975964";
+
 
     constructor(nombre, capital, poblacion) {
         this.nombre = nombre;
         this.capital = capital;
         this.poblacion = poblacion;
+      
     }
 
 
 
-    rellenarAtributosRestantes(formaDeGobierno, coordenadasCapital, religion) {
+    rellenarAtributosRestantes(formaDeGobierno, religion,lat,long) {
         this.formaDeGobierno = formaDeGobierno;
-        this.coordenadasCapital = coordenadasCapital;
         this.religion = religion;
+        this.lat = lat;
+        this.long = long;
+        console.log(`api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=${this.key}`)
     }
 
     obtenerNombreComoTexto() {
@@ -36,14 +35,31 @@ class Pais {
                             <li>Población: ${this.poblacion}</li>
                             <li>Forma de Gobierno: ${this.formaDeGobierno}</li>
                             <li>Religión Mayoritaria: ${this.religion}</li>
+                            <li>Coordenadas: ${this.lat} , ${this.long}</li>
                           </ul>`;
         return listaHTML;
     }
 
 
     obtenerCoordenadasComoHTML(){
-        document.write("<p>10.48801, -66.87919</p>");
+        document.write(`<p>${this.lat}, ${this.long}</p>`);
+    }
 
+    getMeteo(){
+        $.ajax(
+            {
+
+                url:`https://api.openweathermap.org/data/3.0/onecall?lat=${this.lat}&lon=${this.long}&
+                units=metric&exclude=hourly&appid=${this.key}`,
+                method: "GET",
+                dataType: "json",
+                success: function(data){
+
+                },
+                error: function(status){
+                    console.log(status);
+                }
+        });
     }
 
 }
